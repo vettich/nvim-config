5934c622d958c12bc1a1849180a960e6947788e5
Oleg <vetti.ch@ya.ru>
2023-09-09 13:07:08 +0500

    feat(theme): change theme to kanagawa

--- after/plugin/00-colors.lua
+++ after/plugin/00-colors.lua
@@ -2,13 +2,13 @@
 --     disable_background = true
 -- })
 
-function ColorMyPencils(color) 
-	color = color or "tokyonight"
+function ColorMyPencils(color)
+	-- color = color or "tokyonight"
+	color = color or "kanagawa"
 	vim.cmd.colorscheme(color)
 
 	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
 end
 
 ColorMyPencils()
--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,23 +1,25 @@
 require('lualine').setup({
 	options = {
 		icons_enabled = false,
-		theme = 'tokyonight',
+		-- theme = 'tokyonight',
+		-- theme = 'marklar',
+		theme = 'kanagawa',
 		disabled_filetypes = {},
 		always_divide_middle = true,
 	},
 	sections = {
-		lualine_a = {'mode'},
-		lualine_b = {'branch', 'diff', 'diagnostics'},
-		lualine_c = {{'filename', path = 1}},
-		lualine_x = {'encoding', 'fileformat', 'filetype'},
-		lualine_y = {'progress'},
-		lualine_z = {'location'}
+		lualine_a = { 'mode' },
+		lualine_b = { 'branch', 'diff', 'diagnostics' },
+		lualine_c = { { 'filename', path = 1 } },
+		lualine_x = { 'encoding', 'fileformat', 'filetype' },
+		lualine_y = { 'progress' },
+		lualine_z = { 'location' }
 	},
 	inactive_sections = {
 		lualine_a = {},
 		lualine_b = {},
-		lualine_c = {'filename'},
-		lualine_x = {'location'},
+		lualine_c = { 'filename' },
+		lualine_x = { 'location' },
 		lualine_y = {},
 		lualine_z = {}
 	},
@@ -34,5 +36,5 @@ require('lualine').setup({
 	-- 	lualine_y = {},
 	-- 	lualine_z = {}
 	-- },
-	extensions = {'nerdtree'}
+	extensions = { 'nerdtree' }
 })
